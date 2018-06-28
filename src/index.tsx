a49ecc93295a16dd2b75e8d6afe3b224919cc5f7
Alessandro Garzaro <mail@alessandrogarzaro.net>
2018-06-28 13:13:32 +0200

    chore: removed bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom';
 
 import App from './App';
 
-import 'node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
